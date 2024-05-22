2a03b4185604dade9ac1c3b3610db36af1de18a7
YassineOularbi <yassineoularbi4@gmail.com>
2024-05-22 20:36:24 +0100

    Add configuration database

--- src/main/java/org/config/DatabaseConfig.java
+++ src/main/java/org/config/DatabaseConfig.java
@@ -1,4 +1,15 @@
 package org.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConfig {
+    private static final String url = "jdbc:mysql://localhost:3306/construction_db";
+    private static final String username = "root";
+    private static final String password = "1234";
+
+    private static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
 }
