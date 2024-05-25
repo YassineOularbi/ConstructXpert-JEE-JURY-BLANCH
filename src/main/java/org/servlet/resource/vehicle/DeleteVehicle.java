1a3cce5b34dcf2dc4e93ad4be892ad72c73fc0a0
YassineOularbi <yassineoularbi4@gmail.com>
2024-05-25 03:04:20 +0100

    Add methode delete vehicle

--- src/main/java/org/servlet/resource/vehicle/DeleteVehicle.java
+++ src/main/java/org/servlet/resource/vehicle/DeleteVehicle.java
@@ -1,18 +1,30 @@
 package org.servlet.resource.vehicle;
 
+import org.dto.UserDTO;
 import org.service.VehicleService;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "DeleteVehicle", value = "/DeleteVehicle")
 public class DeleteVehicle extends HttpServlet {
     VehicleService vehicleService = new VehicleService();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        Long id = Long.valueOf(request.getParameter("id"));
+        try {
+            vehicleService.delete(id);
+            request.setAttribute("vehicles", vehicleService.getAllVehicles());
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        HttpSession session = request.getSession();
+        UserDTO userDTO = (UserDTO) session.getAttribute("user");
+        request.setAttribute("user", userDTO);
+        this.getServletContext().getRequestDispatcher("/Vehicles.jsp").forward(request, response);
     }
 
     @Override
